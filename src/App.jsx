201eed51ee3c30a2715e8517730a25f82c087540
fajarkusumaa <fajarkusuma46@gmail.com>
2023-12-15 00:20:42 +0700

    add div for pagination

--- src/App.jsx
+++ src/App.jsx
@@ -169,12 +169,16 @@ function App() {
 
                 <div className="peer h-8 w-14 rounded-full bg-slate-600 after:absolute after:start-[4px] after:top-[4px] after:h-6 after:w-6 after:rounded-full after:border-gray-300 after:bg-orange-500 after:transition-all after:content-[''] peer-checked:bg-slate-500 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-slate-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-slate-800"></div>
               </label>
-              <a href="https://github.com/fajarkusumaa">
+              <a
+                href="https://github.com/fajarkusumaa"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="36"
                   height="36"
-                  className="fill-slate-700 ease-in hover:fill-orange-400 dark:fill-slate-500"
+                  className="fill-slate-700 duration-200 ease-in hover:fill-orange-400 dark:fill-slate-500 dark:hover:fill-orange-400"
                   viewBox="0 0 24 24"
                 >
                   <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
@@ -215,9 +219,20 @@ function App() {
                     </div>
                   </a>
                 ))}
+
+                <div className="mt-4 flex gap-2">{/* {list._links.} */}</div>
               </div>
-              <div className="flex items-start gap-2">
-                <button onClick={() => handleNextPage()}>Next</button>
+
+              <div className="w-100 mt-5 text-center text-base font-bold dark:text-white">
+                Support by{" "}
+                <a
+                  href="https://developer.edamam.com/edamam-recipe-api"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-orange-500 hover:text-orange-600"
+                >
+                  Edamam API
+                </a>
               </div>
             </>
           )}
@@ -243,7 +258,7 @@ function App() {
                   </div>
                   {/* Ingredients */}{" "}
                   <h5 className="mt-8 text-2xl text-orange-300">Ingredients</h5>
-                  <div className="mb-8 mt-4 grid h-full w-3/4 grid-cols-6 gap-8 overflow-y-auto ">
+                  <div className="mb-8 mt-4 grid w-3/4 grid-cols-6 gap-8 overflow-y-auto ">
                     {dataItem.recipe.ingredients.map((item, i) => (
                       <div
                         key={i}
@@ -260,7 +275,7 @@ function App() {
                       </div>
                     ))}
                   </div>
-                  <span className="mt-auto">
+                  <span className="mt-auto dark:text-white">
                     Source :{" "}
                     <a
                       href={dataItem.recipe.url}
